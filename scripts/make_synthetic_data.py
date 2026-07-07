45b3f96f6995584324e68e7bb49cdfbc14338079
Konstantin A. Klochkov <konstantin.a.klochkov@gmail.com>
2026-07-07 17:16:49 +0300

    Moved to Okabe-Ito color palette

--- scripts/make_synthetic_data.py
+++ scripts/make_synthetic_data.py
@@ -1,18 +1,8 @@
 from bloodsim import BloodSim
-import pickle
-import types
+import os
 import numpy as np
 import pandas as pd
-
-
-class Blood:
-    def __init__(self, n_day, lab_memory, sim):
-        self.n_day = n_day
-        self.lab_memory = {'crp': lab_memory, 'crp_mean': np.array(blood.pop_blood['crp'].iloc[:, 1:].mean())}
-        self.sim = types.SimpleNamespace()
-        self.sim.results = types.SimpleNamespace()
-        self.sim.results.new_infections = np.array(sim.results.new_infections)
-        pass
+from concurrent.futures import ProcessPoolExecutor
 
 
 def make_input_file(n_days = 300, n_dayperson = 250):
@@ -24,15 +14,38 @@ def make_input_file(n_days = 300, n_dayperson = 250):
     return pd.DataFrame(n_dayperson_dict)
 
 
-if __name__=='__main__':
+def run_seed(args):
+    i, n_dayperson = args
     pop_size = int(1e6)
-    n_dayperson = 1250
-    seeds = 1
-    
-    for i in range(seeds):
-        blood = BloodSim(n_person_per_day = make_input_file(n_days = 300, n_dayperson = n_dayperson), end_day = 300, pop_size = pop_size, guest_strategy = 'random', rand_seed = i)
-        blood.sim_run()
-        lossy_blood = Blood(n_day = len(blood.lab_memory['crp'].keys()), lab_memory = blood.lab_memory['crp'], sim = blood.sim)
+    n_days = 300
 
-        with open(f'synthetic_data/synthetic_blood_{n_dayperson}_{i}.pkl', 'wb') as f:
-            pickle.dump(lossy_blood, f)
+    blood = BloodSim(n_person_per_day = make_input_file(n_days = n_days, n_dayperson = n_dayperson), end_day = n_days, pop_size = pop_size, guest_strategy = 'random', rand_seed = i)
+    blood.sim_run()
+
+    crp = pd.DataFrame(blood.lab_memory['crp']).melt(var_name='day', value_name='crp')
+    crp['seed'] = i
+
+    new_infections = pd.DataFrame({
+        'day': np.arange(len(blood.sim.results.new_infections)),
+        'new_infections': np.array(blood.sim.results.new_infections),
+        'seed': i,
+    })
+
+    return crp, new_infections
+
+
+if __name__=='__main__':
+    seeds = 1000
+    workers = 25
+    n_dayperson = 1250
+    
+    os.makedirs('synthetic_data', exist_ok=True)
+
+    with ProcessPoolExecutor(max_workers = workers) as executor:
+        results = list(executor.map(run_seed, [(i, n_dayperson) for i in range(seeds)]))
+
+    crp = pd.concat([x[0] for x in results], ignore_index=True)
+    new_infections = pd.concat([x[1] for x in results], ignore_index=True)
+
+    crp.to_csv(f'synthetic_data/crp_{n_dayperson}.csv', index=False)
+    new_infections.to_csv(f'synthetic_data/new_infections_{n_dayperson}.csv', index=False)
